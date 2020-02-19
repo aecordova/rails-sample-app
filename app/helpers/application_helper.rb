2ca84547d3e3b5ce9994b0a1298adda65807b9d9
acordova <ae_cordova@outlook.com>
2020-02-19 14:02:18 -0600

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Custom Helpers
 module ApplicationHelper
+  # Returns the full title per page
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return page_title + ' | ' + base_title unless page_title.empty?
+
+    base_title
+  end
 end
