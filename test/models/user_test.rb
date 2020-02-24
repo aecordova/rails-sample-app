4157504c7fd458525751efd17eb5fee35b772d40
acordova <ae_cordova@outlook.com>
2020-02-24 15:51:08 -0600

    Added secure password to users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup 
-    @user = User.new(name:"TestUser",email:"test@email.com")
+    @user = User.new(name:"TestUser",email:"test@email.com",
+                    password: "123456", password_confirmation:"123456")
   end
 
   test "should be valid" do
@@ -12,25 +13,39 @@ class UserTest < ActiveSupport::TestCase
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
-  end 
+  end
+
   test "email should be present" do
     @user.email = "     "
     assert_not @user.valid?
   end
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
+
   test "email addresses should be saved as lower case" do
     mixed_case_email = "Foo@ExaMpLe.com"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+  
+  test "passwords should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "passwords should have a min length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end 
 end
