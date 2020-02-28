e1298e80072f18ec7757ff7be35003fd109667ff
acordova <ae_cordova@outlook.com>
2020-02-28 14:54:47 -0600

    Added methods for user login and for current user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,17 @@
 class SessionsController < ApplicationController
-  def new
-  end
   
+  include SessionsHelper ##Why do I have to include it?
+  
+  def new; end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #login user
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'  
+      render 'new'
     end
   end
 
