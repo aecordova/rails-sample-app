5821bf85c2de3041b1c3528ae459470f82421bea
acordova <ae_cordova@outlook.com>
2020-03-02 15:54:43 -0600

    Added working menus with bootstrap and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@
 
 // require("jquery")
 // require("bootstrap")
+require("bootstrap/dist/js/bootstrap")
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
