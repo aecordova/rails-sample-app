8d644b9d463a8fef8c3d6ab3e9012c2d07678c5b
acordova <ae_cordova@outlook.com>
2020-03-03 16:35:08 -0600

    Added modifications on sessions controller and helper to add remember token and session id to cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,17 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def remember
-    self.remember_token = new_token
-    update_attribute(:remember_digest, digest(remember_token))
-  end
-
   def self.new_token
     SecureRandom.urlsafe_base64
   end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? (remember_token)
+    BCrypt::Password.new(:remember_digest) == remember_token
+  end
+
 end
