6f0c5821bd0acfc61ff6e73e201b99c98528185e
acordova <ae_cordova@outlook.com>
2020-03-12 20:25:28 -0600

    Added password reset methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
