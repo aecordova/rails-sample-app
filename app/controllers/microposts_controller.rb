6b50dc7ca90e26936cdadfed4b4e899bc7bb652c
acordova <ae_cordova@outlook.com>
2020-03-16 12:56:58 -0600

    Added microposts form and updated all views accordingly

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,23 @@
 class MicropostsController < ApplicationController
   before_action :only_logged_in, only:[:create, :destroy]
 
-  def create;  end
-  def destroy;  end
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy; end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+
 
 end
